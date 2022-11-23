113a4994ed6fe5aa6b69c1449dcdb2a0b3372e5d
njnaste <116603638+njnaste@users.noreply.github.com>
2022-11-23 11:31:27 +0200

    Site Criado

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function a()
+{
+
+    let dia = new Date()
+    let hora = dia.getHours()
+    let men = dia.getMinutes()
+    let sec = dia.getSeconds()
+
+    alert('em construcao '+hora+':'+men+''+sec)
+
+}
